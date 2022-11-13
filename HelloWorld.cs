0ebff019136ca2c183661be3a71232b6f65a80bc
vipgost0 <118118211+vipgost0@users.noreply.github.com>
2022-11-13 17:56:15 +0300

    добавил script

---
+++ HelloWorld.cs
@@ -0,0 +1,18 @@
+
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // 01000101011101010001
+    void Start()
+    {
+        print("Hello World");
+        print(123);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
